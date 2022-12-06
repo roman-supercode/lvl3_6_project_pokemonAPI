d7a0ffcb66e9596892faf20982cfdc851679471e
roman-supercode <roman.supercode@gmail.com>
2022-12-06 11:35:51 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from './pages/Detail';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+
 
 function App() {
-  return;
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/detail/:name' element={<Detail />}></Route>
+        <Route path='/menu' element={<Menu />}></Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
