b805afc26a8770fe8cf65e36c9a8e010f1b344c2
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-06 20:42:40 +0100

    began styling. needs font-family (Roboto into fonts folder on main). added moves. sort moves button needs troubleshooting.

--- src/components/detailcard/DetailCard.js
+++ src/components/detailcard/DetailCard.js
@@ -7,6 +7,9 @@ import "./DetailCard.css";
 const DetailCard = () => {
     const params = useParams();
     const [character, setCharacter] = useState([]);
+    // const [sortMove, setSortMove] = useState();
+    // const [alpha, setAlpha] = useState(false);
+    // let moves = [];
 
     useEffect(() => {
         fetch(`https://pokeapi.co/api/v2/pokemon/${params.name}`)
@@ -18,16 +21,51 @@ const DetailCard = () => {
 
     if (character.name === undefined) return;
 
-    console.log(character)
+    const FormatId = () => {
+        if (character.id < 10) {
+            return <p>#00{character.id}</p>
+        } else if (character.id < 100) {
+            return <p>#0{character.id}</p>
+        } else {
+            return <p>#{character.id}</p>
+        }
+    }
+    // to test: Mew (151), bulbasaur (1), onix (95)
+
+
+    //SORT moves alphabetically
+    // const handleSortMove = () => {
+    //     character.moves.map((move) => {
+    //         moves.push(move);
+    //     })
+
+    //     console.log(moves[0].move.name)
+
+    //     setAlpha(current => !current);
+    //     alpha ? sortMove.sort((a, b) => a.move.name - b.move.name) : sortMove.sort((a, b) => b.move.name - a.move.name);
+    //     setSortMove(sortMove)
+    // }
+
     return (<div className="detailCardDiv">
         <div className="characterBackgroundDiv">
             <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${character.id}.svg`} alt={character.name} />
         </div>
-        <p>{character.name}</p>
+        <div className="characterIdNameDiv">
+            <FormatId />
+            <p>{character.name}</p>
+        </div>
         {character.types.map((singleType, index) => {
             return <p key={index}>{singleType.type.name}</p>
         })
         }
+        <p className="attacksAndMovementsP">ATTACKS AND MOVEMENTS</p>
+        {/* onClick={handleSortMove} */}
+        <button>Sort moves</button>
+        <div className="attacksAndMovementsDiv">
+            {character.moves.map((singleMove, index) => {
+                return <p className="moveP" key={index}>{singleMove.move.name}</p>
+            })}
+        </div>
     </div>);
 }
 
