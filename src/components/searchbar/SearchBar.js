da03a316c128de7c7ba99e5cf846334d8aabf028
MoSuperCode <mo.supercode@gmail.com>
2022-12-08 11:30:57 +0100

    dayNight

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,5 +1,5 @@
 import './SearchBar.css';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import headingPic from '../../assets/img/heading.svg';
 import daynight from '../../assets/img/daynight.svg';
@@ -7,14 +7,23 @@ import hamburger from '../../assets/img/hamburger.svg';
 
 export default function SearchBar({ search }) {
     const [darkmode, setDarkMode] = useState(false);
+
     let dayNight = () => darkmode ? setDarkMode(false) : setDarkMode(true);
+    useEffect(() => {
+        if (darkmode == true) {
+            document.body.style = 'background: #000000';
+        } else {
+            document.body.style = 'background: #CCDADD';
+        }
+    }, [darkmode]);
+
     return (
         <div className='searchBarContainer'>
             <Link to={"/"}><img className='logoImg' src={headingPic}></img></Link>
             <div className='flexContainer'>
                 <Link to={"/menu"}><img src={hamburger}></img></Link>
                 <input placeholder='Pokemon name' onChange={(e) => search(e.target.value)}></input>
-                <img src={daynight}></img>
+                <img onClick={dayNight} src={daynight}></img>
             </div>
         </div>
     );
