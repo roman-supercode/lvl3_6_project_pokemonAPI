317c349082d1fc7e4487f73feeea6f5d6f61d17c
MoSuperCode <mo.supercode@gmail.com>
2022-12-08 11:03:08 +0100

    .

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -1,12 +1,13 @@
 import './SearchBar.css';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import headingPic from '../../assets/img/heading.svg';
 import daynight from '../../assets/img/daynight.svg';
 import hamburger from '../../assets/img/hamburger.svg';
 
 export default function SearchBar({ search }) {
-
+    const [darkmode, setDarkMode] = useState(false);
+    let dayNight = () => darkmode ? setDarkMode(false) : setDarkMode(true);
     return (
         <div className='searchBarContainer'>
             <Link to={"/"}><img className='logoImg' src={headingPic}></img></Link>
