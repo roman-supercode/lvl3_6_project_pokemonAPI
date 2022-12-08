0dfd293ab3ca27b0cee2705907278ef13c98e570
MoSuperCode <mo.supercode@gmail.com>
2022-12-08 09:05:23 +0100

    searchbar in all Pokemon

--- src/components/searchbar/SearchBar.js
+++ src/components/searchbar/SearchBar.js
@@ -4,14 +4,14 @@ import headingPic from '../../assets/img/heading.svg';
 import daynight from '../../assets/img/daynight.svg';
 import hamburger from '../../assets/img/hamburger.svg';
 
-export default function SearchBar() {
+export default function SearchBar({ search }) {
     const inputref = useRef();
     return (
         <div className='searchBarContainer'>
             <img className='logoImg' src={headingPic}></img>
             <div className='flexContainer'>
                 <img src={hamburger}></img>
-                <input placeholder='Pokemon name' ref={inputref}></input>
+                <input placeholder='Pokemon name' onChange={(e) => search(e.target.value)}></input>
                 <img src={daynight}></img>
             </div>
         </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,45 @@
 
 import React, { useEffect, useState } from 'react';
 import HomeCard from '../components/homecard/HomeCard';
-import Searchbar from "../components/searchbar/SearchBar"
+import Searchbar from "../components/searchbar/SearchBar";
 
 const Home = () => {
     const [allPokemon, setAllPokemon] = useState();
+    const [searchTerm, setSearchTerm] = useState();
+    const [useAbleData, setuseAbleData] = useState();
 
     useEffect(() => {
         fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100")
             .then(response => response.json())
             .then(allPokemonAPI => {
                 setAllPokemon(allPokemonAPI);
+                setuseAbleData(allPokemonAPI.results);
             });
     }, []);
 
+    function searchFunction(search) {
+        setSearchTerm(search);
+    }
 
-    if (allPokemon === undefined) return;
+
+    useEffect(() => {
+        if (allPokemon === undefined) {
+            return;
+        }
+        if (searchTerm === "") {
+            setuseAbleData(allPokemon);
+        }
+        // setSearchTerm(pokemon/${searchTerm});
+        let length = (searchTerm).length;
+
+        setuseAbleData(allPokemon.results.filter(el => el.name.slice(0, length).toLowerCase() === (searchTerm).toLowerCase().replaceAll(" ", "-")));
+
+    }, [searchTerm]);
+
+
+
+
+
 
 
     // ================= Bilder Version ==============
@@ -26,21 +50,23 @@ const Home = () => {
 
     // ==============================================
 
+    if (allPokemon === undefined) return;
 
 
     return (
         <div className='home'>
 
-            <Searchbar />
+            <Searchbar search={searchFunction} />
             <div className='homecards'>
-                {allPokemon.results.map((object, index) => {
+                {useAbleData.map((object, index) => {
+                    let i = object.url.slice(-6, -1).replace("/", "").replace("n", "").replace("o", "").replace("m", "");
                     return (
 
                         <HomeCard
                             name={object.name}
                             key={index}
-                            imgURL={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${index + 1}.png`}
-                            id={index + 1}
+                            imgURL={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${i}.png`}
+                            id={i}
                         />);
                 })}
 
